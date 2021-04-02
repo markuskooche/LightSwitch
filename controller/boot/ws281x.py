17c4e28c7650b72e8717686f93b214ea372310f4
Valentin Falke <vale.555@icloud.com>
2021-04-02 18:27:55 +0200

    added set_rgb_color method and renamed _set_color to reduce conflicts

--- controller/boot/ws281x.py
+++ controller/boot/ws281x.py
@@ -14,6 +14,9 @@ class Ws281xController:
     def set_hex_color(self,hexColor):
         self.current_rgb = tuple(int(hexColor[i:i+2], 16) for i in (0, 2, 4))
         self._update_state()
+    
+    def set_rgb_color(self ,r ,g ,b):
+        self.current_rgb = (r,g,b)
 
     def set_is_on(self,is_on):
         self.is_on = is_on
@@ -21,13 +24,13 @@ class Ws281xController:
 
     def _update_state(self):
         if (self.is_on):
-            self._set_color(self.current_rgb[0],self.current_rgb[1],self.current_rgb[2])
+            self._set_pixels(self.current_rgb[0],self.current_rgb[1],self.current_rgb[2])
         else:
-            self._set_color(0,0,0)
+            self._set_pixels(0,0,0)
 
 #MARK: set colors
 
-    def _set_color(self,r, g, b):
+    def _set_pixels(self,r, g, b):
         for i in range(self.number_of_pixels):
             self.pixels[i] = (r, g, b)
         self.pixels.write()
