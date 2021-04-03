9fa0ea436593747b5876a9d814ca9b631bf9ef2d
Valentin Falke <vale.555@icloud.com>
2021-04-03 15:44:46 +0200

    added robust MQTTClient subclass

--- controller/boot/mqtt.py
+++ controller/boot/mqtt.py
@@ -23,12 +23,12 @@
 
 import usocket as socket
 import ustruct as struct
+import utime
 from ubinascii import hexlify
-
 class MQTTException(Exception):
     pass
 
-class MQTTClient:
+class SimpleMQTTClient:
 
     def __init__(self, client_id, server, port=0, user=None, password=None, keepalive=0,
                  ssl=False, ssl_params={}):
@@ -225,3 +225,45 @@ class MQTTClient:
     def check_msg(self):
         self.sock.setblocking(False)
         return self.wait_msg()
+
+
+class MQTTClient(SimpleMQTTClient):
+
+    DELAY = 2
+    DEBUG = False
+
+    def delay(self, i):
+        utime.sleep(self.DELAY)
+
+    def log(self, in_reconnect, e):
+        if self.DEBUG:
+            if in_reconnect:
+                print("mqtt reconnect: %r" % e)
+            else:
+                print("mqtt: %r" % e)
+
+    def reconnect(self):
+        i = 0
+        while 1:
+            try:
+                return super().connect(False)
+            except OSError as e:
+                self.log(True, e)
+                i += 1
+                self.delay(i)
+
+    def publish(self, topic, msg, retain=False, qos=0):
+        while 1:
+            try:
+                return super().publish(topic, msg, retain, qos)
+            except OSError as e:
+                self.log(False, e)
+            self.reconnect()
+
+    def wait_msg(self):
+        while 1:
+            try:
+                return super().wait_msg()
+            except OSError as e:
+                self.log(False, e)
+            self.reconnect()
