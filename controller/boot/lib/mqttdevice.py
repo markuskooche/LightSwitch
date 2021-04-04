6e8051980411b9d9e1fe5fe5241c1f4667162b6c
Valentin Falke <vale.555@icloud.com>
2021-04-04 21:11:34 +0200

    added MQTTDevice abstract class this will reduce the code redundancy once implemented in Ws281x and MultiwaySwitch

---
+++ controller/boot/lib/mqttdevice.py
@@ -0,0 +1,39 @@
+import utime
+from mqtt import MQTTClient
+
+
+MQTT_CLIENT_ID_RANDOM = "a8b3"
+MQTT_BROKER_ADDRESS = "raspberrypi"
+
+MQTT_PORT = 1883
+KEEP_ALIVE_TIME_SEC = 600
+PING_EVERY_SEC = KEEP_ALIVE_TIME_SEC * 0.9
+
+class MQTTDevice:
+
+    def __init__(self,topic_online,true_message="true",false_message="false"):
+        self.true_message = true_message
+        self.false_message = false_message
+        self.client = MQTTClient(MQTT_CLIENT_ID_RANDOM, MQTT_BROKER_ADDRESS, port=MQTT_PORT, keepalive=KEEP_ALIVE_TIME_SEC)
+        self.next_scheduled_ping_time = 0
+        self._client_setup(topic_online)
+    
+    def _client_setup(self,topic_online):
+        self.client.set_last_will(topic_online, self.false_message)
+        self.client.connect()
+        self.client.publish(topic_online, self.true_message)
+        self.setup_subscriptions()
+
+    # override this method in subclass
+    # set callback and subscriptions
+    def setup_subscriptions(self):
+        pass
+    
+    def _ping(self):
+        if (self.next_scheduled_ping_time < utime.time() ):
+            self.client.ping()
+            self.next_scheduled_ping_time = utime.time() + PING_EVERY_SEC
+
+    def run(self):
+        self._ping()
+        self.client.check_msg()
